df570496c753adee591b7d2f54df0429d27c5b7e
Judy <judy.huang@rstn.com.tw>
2025-04-26 10:01:12 +0800

    Refactor install script to streamline Python installation process

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-
-
 # 安装 Homebrew（如果未安装）
 if ! command -v brew &>/dev/null; then
   echo "Homebrew 未安装。正在安装 Homebrew..."
@@ -18,16 +16,16 @@ echo 'eval "$(pyenv init --path)"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
 
+# 安装 Python 3.12.2
+pyenv install 3.12.2
+pyenv global 3.12.2
+
 # 重新加载 shell
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-
-# 安装 Python 3.12.2
-pyenv install 3.12.2
-pyenv global 3.12.2
 
 # 确认安装
 python --version
